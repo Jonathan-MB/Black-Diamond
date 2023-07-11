5142d78c333702c4ecaabb614935d45ff630c3b6
Santiago <herrerafernandezsantiago@gmail.com>
2023-07-10 19:21:21 -0500

    Cabecera

--- resources/views/cabecera.blade.php
+++ resources/views/cabecera.blade.php
@@ -1,78 +1,51 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="icon" type="image/png" href="{{ asset('img/iconoBD.ico') }}">
-        <script src="{{ asset('js/jquery-3.6.4.min.js') }}"></script>
-        <script src="{{ asset('js/general.js') }}"></script>
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Black Diamond</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+        <link rel="stylesheet" href="css/head.css" />
     </head>
-    <body>
-
-        <header class="cabecera">
-{{-- logo de la empresa --}}
-            <a  class="logo" href="{{ asset("/") }}">
-                <img src="{{ asset('img/logo.png') }}" alt="Logo de Black Diamond">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background: #000000;">
+        <div class="container-fluid">
+            <a id="logo" class="navbar-brand" href="#">
+                <img src="{{url('img/logo.png')}}" alt="Logo" height="50" width="250" />
             </a>
-
-{{-- logo para celular --}}
-            <a  class="logo2" href="{{ asset("/") }}">
-                <img src="{{ asset('img/logo2.png') }}" alt="Logo de Black Diamond">
-            </a>
-
-{{-- buscador --}}
-            <div class="buscador">
-                <input type="text" class="textBuscar" placeholder="Buscar...">
-                <div class="buscar">
-                    <img src="{{ asset('img/lupa.png') }}" alt="luoa">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse show" id="navbarScroll">
+                <div class="navbar-collapse collapse show" id="navbarScroll">
+                    <ul class="navbar-nav mx-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">HOMBRE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">MUJER</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ZAPATOS </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">MARCA</a>
+                        </li>
+                    </ul>
+                    <form id="search-form" class="d-flex" role="search">
+                        <input class="form-control me-1" type="search" placeholder="Buscar" />
+                        <a>
+                            <img width="40" height="40" src="https://img.icons8.com/ios/50/search--v1.png" alt="search--v1" />
+                        </a>
+                    </form>
+                    <a href="">
+                        <img width="32" height="32" src="{{url('img/usuario.png')}}" alt="user" />
+                    </a>
+                    <a href="">
+                        <img width="32" height="32" src="{{url('img/icons8-shopping-cart-48.png')}}" alt="cart" />
+                    </a>
                 </div>
             </div>
-
-{{-- carrito y login --}}
-            <div class="carritoylogin">
-                <a class="contenedorIcono"  href="{{ asset("/carrito") }}">
-                    <img class="icono" src="{{ asset('img/carrito.png') }}" alt="carrito">
-                </a>
-
-                <a class="contenedorIcono"  href="{{ asset("/login") }}">
-                    <img class="icono" src="{{ asset('img/usuario.png') }}" alt="carrito">
-                </a>
-            
-{{-- menu burger para cel --}}
-                <a  class="menuPlegable" href="{{ asset("/") }}">
-                    <img src="{{ asset('img/menuBurger.png') }}" alt="Logo de Black Diamond">    
-                </a>
-            </div>
-            
-        </header>
-    
-    <nav class="categorias">
-        <div class="seleccionComun">
-            <img src="{{ asset('img/hombres.png') }}" alt="hombres"> <br>
-            <p>Hombres</p>
-        </div>
-        <div class="seleccionComun">
-            <img src="{{ asset('img/mujeres.png') }}" alt="mujeres"> <br>
-            <p>Mujeres</p>
-        </div>
-        
-        <div class="seleccionComun">
-            <img src="{{ asset('img/ninos.png') }}" alt="niños"> <br>
-            <p>Niños</p>
-        </div>
-        <div class="seleccionComun">
-            <img src="{{ asset('img/zapatos.png') }}" alt="zapatos"> <br>
-            <p>Calzado</p>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         </div>
     </nav>
-            @yield('contenido')
-        <footer class="piePagina">
-            <div>terminos y condiciones</div>
-            <div>referencias</div>       
-        </footer>
-    </body>
 </html>
